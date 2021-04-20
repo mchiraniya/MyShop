db2b5735a0bf22928b3b19bec1e8823c200f7e50
Mohit Chiraniya <mchiraniya@outlook.com>
2021-04-20 16:24:35 +0530

    SQLRepository

---
+++ MyShop.DataAccess.SQL/SQLRepository.cs
@@ -0,0 +1,58 @@
+﻿using MyShop.core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyShop.core.Models;
+using System.Data.Entity;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class SQLRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        internal DataContext context;
+        internal DbSet<T> dbSet;
+
+        public SQLRepository(DataContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<T>();
+        }
+
+        public IQueryable<T> Collection()
+        {
+            return dbSet;
+        }
+
+        public void Commit()
+        {
+            context.SaveChanges();
+        }
+
+        public void Delete(string id)
+        {
+            T t = Find(id);
+            if(context.Entry(t).State == EntityState.Detached)
+                dbSet.Attach(t);
+
+            dbSet.Remove(t);
+        }
+
+        public T Find(string id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public void Insert(T p)
+        {
+            dbSet.Add(p);
+        }
+
+        public void Update(T t)
+        {
+            dbSet.Attach(t);
+            context.Entry(t).State = EntityState.Modified;
+        }
+    }
+}
