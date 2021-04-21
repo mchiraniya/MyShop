f00744e0aa75bd0408d4272b5aeafb06e1e98e83
Mohit Chiraniya <mchiraniya@outlook.com>
2021-04-21 09:46:59 +0530

    BasketService

---
+++ MyShop.core/Interfaces/IBasketService.cs
@@ -0,0 +1,18 @@
+﻿using MyShop.core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyShop.core.Interfaces
+{
+    public interface IBasketService
+    {
+        void AddToBasket(HttpContextBase httpContextBase, string productId);
+        void RemoveFromBasket(HttpContextBase httpContextBase, string basketItemId);
+        List<BasketItemViewModel> GetBasketItems(HttpContextBase httpContextBase);
+        BasketSummaryViewModel GetBasketSummary(HttpContextBase httpContextBase);
+    }
+}
